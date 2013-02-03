d6525c2d7943b814d5a782e92dee0e3f60572d9c
Donald Huang <don.hcd@gmail.com>
2013-02-03 05:27:42 -0500

    added default code

--- hi.rb
+++ hi.rb
@@ -2,10 +2,26 @@ require 'sinatra'
 require 'cgi'
 require 'open-uri'
 
+default_code =
+  "int fib(int i) {
+  int f1=0;
+  int f2=1;
+  for (int q=0; q<i-1; q++) {
+    int tmp = f2;
+    f2 = f1+f2;
+    f1 = tmp;
+  }
+  return f1;
+}
+int main(){
+  printf(\"%d\",fib(7));
+  return 0;
+}
+"
 
 get '/' do
   erb :index, :locals => {
-    :ccode => ''
+    :ccode => default_code
   }
 end
 
