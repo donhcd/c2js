f1971d68b4d7d3508833b5c3448daf22f9f889ff
Andrew Audibert <aaudiber@andrew.cmu.edu>
2013-02-03 02:47:01 -0500

    stuff

---
+++ hi.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'cgi'
+require 'open-uri'
+
+
+get '/' do
+  erb :index
+end
+
+get '/dbdl' do
+  url = CGI.unescape(params[:url])
+  result = open(url, 'rb').read()
+  puts(url)
+  puts(result);
+end
